5e3e264365b1178b8000a7266b524653a5099109
CarlosDaniel0 <carlos.daniel2016@hotmail.com>
2021-10-01 20:50:12 -0300

    Ultima versao geckodriver

--- install-geckodriver.sh
+++ install-geckodriver.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-INSTALL_DIR="/usr/local/bin"
-VERSION=v0.29.1
+install_dir="/usr/local/bin"
 
 echo "Start Install of geckodriver"
-wget https://github.com/mozilla/geckodriver/releases/download/$VERSION/geckodriver-$VERSION-linux64.tar.gz
-tar -xvzf geckodriver*
-chmod +x geckodriver
-mv geckodriver "$INSTALL_DIR"
+json=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest)
+if [[ $(uname) == "Darwin" ]]; then
+    url=$(echo "$json" | jq -r '.assets[].browser_download_url | select(contains("macos"))')
+elif [[ $(uname) == "Linux" ]]; then
+    url=$(echo "$json" | jq -r '.assets[].browser_download_url | select(contains("linux64"))')
+else
+    echo "can't determine OS"
+    exit 1
+fi
+
+url=$(echo $url | cut -f1 -d" ")
+curl -s -L "$url" | tar -xz
+chmod +x geckodriver*
+sudo mv geckodriver "$install_dir"
 rm -f geckodriver*
-echo "installed geckodriver binary in $INSTALL_DIR"
+echo "installed geckodriver binary in $install_dir"
